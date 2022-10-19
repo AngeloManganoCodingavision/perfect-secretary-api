dcdd2a05e544a9aa9c34d795e4356f4ec2938de2
Angelo <angelo.mangano@develhope.co>
2022-10-19 09:10:24 +0200

    add firebase

---
+++ firebase.ts
@@ -0,0 +1,23 @@
+import { cert, initializeApp, ServiceAccount } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
+
+const serviceAccount = {
+  type: "service_account",
+  project_id: "perfect-secretary-2ff60",
+  private_key_id: "db26b3a569276659a58f53963a37a7006666ff32",
+  private_key: process.env.FIREBASE_SECRET_KEY,
+  client_email: "firebase-adminsdk-ky67r@perfect-secretary-2ff60.iam.gserviceaccount.com",
+  client_id: "115579771995354304788",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-ky67r%40perfect-secretary-2ff60.iam.gserviceaccount.com"
+};
+
+initializeApp({
+  credential: cert(serviceAccount as ServiceAccount)
+});
+
+const db = getFirestore();
+
+export default db;
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
 import express  from "express";
 import 'express-async-errors';
+import db from '../firebase';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.json({'id': 'ciaone'})
+app.get('/', async (req, res) => {
+    const snapshot = await db.collection('patients').get();
+    let list: any[] = [];
+    snapshot.forEach((doc) => {
+        list.push(doc.data());
+    });
+    res.status(200).json(list)
 })
 
 export default app;
