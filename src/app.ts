37f720967f9e6302f253eee24f2a1ff4f5473c79
Angelo <angelo.mangano@develhope.co>
2022-10-19 19:14:56 +0200

    add patient crud

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,71 @@
 import express  from "express";
 import 'express-async-errors';
+import { FieldValue } from "firebase-admin/firestore";
 import db from '../firebase';
+import { Patient } from "./models/Patients";
 
 const app = express();
+const patientsRef = db.collection('patients');
+const eventsRef = db.collection('events');
 
-app.get('/', async (req, res) => {
-    const snapshot = await db.collection('patients').get();
-    let list: any[] = [];
-    snapshot.forEach((doc) => {
-        list.push(doc.data());
-    });
-    res.status(200).json(list)
-})
+app.use(express.json());
+
+app.get('/patient', (req, res) => {
+    patientsRef.where("active", "==", true).get()
+    .then((response) => {
+        let list: Patient[] = [];
+        response.forEach((patient) => {
+            list.push({...patient.data() as Patient, id: patient.id });
+        });
+        res.status(200).json(list);
+    })
+    .catch((error) => {
+        res.send(error);
+    })
+});
+
+app.post('/patient', (req, res) => {
+    patientsRef.add({...req.body})
+    .then((newPatient) => {
+        res.status(200).send(newPatient.id);
+    })
+    .catch((error) => {
+        res.send(error);
+    })
+});
+
+app.patch('/patient/:id', (req, res) => {
+    console.log(req.body);    
+    patientsRef.doc(req.params.id).update({
+        address: req.body.address,
+        birthdate: req.body.birthdate,
+        birthplace: req.body.birthplace,
+        city: req.body.city,
+        email: req.body.email,
+        fiscalCode: req.body.fiscalCode,
+        gender: req.body.gender,
+        lastname: req.body.lastname,
+        name: req.body.name,
+        phoneNumber: req.body.phoneNumber,
+        postalCode: req.body.postalCode,
+        updated: FieldValue.serverTimestamp()
+    })
+    .then(() => {
+        res.status(200).send({...req.body, id: req.params.id});
+    })
+    .catch((error) => {
+        res.send(error);
+    })
+});
+
+app.delete('/patient/:id', (req, res) => {
+    patientsRef.doc(req.params.id).delete()
+    .then(() => {
+        res.status(200).send('Il paziente è stato correttamente eliminato');
+    })
+    .catch((error) => {
+        res.send(error);
+    })
+});
 
 export default app;
---
+++ src/models/Patients.ts
@@ -0,0 +1,17 @@
+export interface Patient {
+    active: boolean;
+    added: Date;
+    address: string;
+    birthdate: string;
+    birthplace: string;
+    city: string;
+    email: string;
+    fiscalCode: string;
+    gender: string;
+    id?: string;
+    lastname: string;
+    name: string;
+    phoneNumber: number;
+    postalCode: number;
+    updated: Date;
+}
