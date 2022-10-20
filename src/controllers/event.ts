7913083f0267cf8c1af17d653670dd517574d780
Angelo <angelo.mangano@develhope.co>
2022-10-20 20:34:13 +0200

    minor fix

--- src/controllers/event.ts
+++ src/controllers/event.ts
@@ -13,7 +13,7 @@ export const getAllEvents = (req: any, res: any) => {
             const eventDate = new Timestamp(event.data().start.seconds, event.data().start.nanoseconds).toDate();           
             referencedEvents.push({...event.data() as CalendarEvent, start: eventDate, id: event.id});
         })
-        const eventList = await getUserData(referencedEvents);       
+        const eventList: CalendarEvent[] = await getUserData(referencedEvents);       
         res.status(200).json(eventList);
     })
     .catch((error) => {
@@ -21,28 +21,33 @@ export const getAllEvents = (req: any, res: any) => {
     })
 };
 
-async function getUserData(referencedEvents: CalendarEvent[]) {
-    const eventList = [] as CalendarEvent[];
-    await Promise.all(
-        referencedEvents.map(async (event) => {
-            const userData = await patientsRef.doc(event.patientRef.path).get();           
-            const patientRef = {...userData.data(), id: userData.id};
-            const referencedEvent = {...event, patientRef};
-            eventList.push(referencedEvent);
-        })
-    );
-
-    return eventList;
+async function getUserData(referencedEvents: CalendarEvent[]): Promise<CalendarEvent[]> {
+    try {
+        const eventList = [] as CalendarEvent[];
+        await Promise.all(
+            referencedEvents.map(async (event) => {
+                const userData = await patientsRef.doc(event.patientRef.path).get();               
+                const patientRef = {...userData.data(), id: userData.id};
+                const referencedEvent = {...event, patientRef};
+                eventList.push(referencedEvent);
+            })
+        );
+    
+        return eventList;
+    } catch(error: any) {
+        throw new Error(error);
+    }
 }
 
 export const addEvent = (req: any, res: any) => {
-    eventsRef.add({...req.body})
+    const eventReq = {...req.body, added: new Date(req.body.added), start: new Date(req.body.start), patientRef: db.doc(req.body.patientRef)}
+    eventsRef.add(eventReq)
     .then((newEvent) => {
-      const event = {...req.body, id: newEvent.id};
-      return getUserData(event);
+      const event: CalendarEvent = {...eventReq, id: newEvent.id};
+      return getUserData([event]);
     })
     .then((referencedEvent) => {      
-        res.status(200).json(referencedEvent);
+        res.status(200).json(referencedEvent[0]);
     })
     .catch((error) => {
         res.status(500).send(error);
